1fd3bf2c40dbdd0d20b76a69c5ef1e962862fbfb
devzwide <nxumalobukeka66@gmail.com>
2026-05-22 11:24:20 +0200

    fix duplicate variable declaration

--- infra/modules/network/variables.tf
+++ infra/modules/network/variables.tf
@@ -26,28 +26,6 @@ variable "resource_group_name" {
   type        = string
 }
 
-variable "location" {
-  description = "The Azure region where resources will be created."
-  type        = string
-  default     = "southafricanorth"
-}
-
-variable "environment" {
-  description = "The environment for which the infrastructure is being provisioned."
-  type        = string
-  default     = "dev"
-}
-
-variable "tags" {
-  description = "A map of tags to apply to all resources."
-  type        = map(string)
-  default = {
-    environment = "dev"
-    project     = "mrpapplication"
-    owner       = "devzwide"
-  }
-}
-
 variable "virtual_network_name" {
   description = "The name of the virtual network."
   type        = string
