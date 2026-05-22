3b2ae6497bc3b536afa02068a846f432c34824ae
devzwide <nxumalobukeka66@gmail.com>
2026-05-22 11:21:52 +0200

    fix argument name 'resource_group_name' that is not expected

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -1,10 +1,9 @@
 resource "azurerm_virtual_network" "vnet_mrpapplication" {
   name                = var.virtual_network_name
   location            = var.location
-  environment         = var.environment
   tags                = var.tags
   address_space       = [var.virtual_network_address_space]
-  resource_group_name = azurerm_resource_group.rg_mrpapplication.name
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_subnet" "subnet_app" {
--- infra/modules/network/variables.tf
+++ infra/modules/network/variables.tf
@@ -21,6 +21,33 @@ variable "tags" {
 }
 
 
+variable "resource_group_name" {
+  description = "The name of the resource group to create."
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region where resources will be created."
+  type        = string
+  default     = "southafricanorth"
+}
+
+variable "environment" {
+  description = "The environment for which the infrastructure is being provisioned."
+  type        = string
+  default     = "dev"
+}
+
+variable "tags" {
+  description = "A map of tags to apply to all resources."
+  type        = map(string)
+  default = {
+    environment = "dev"
+    project     = "mrpapplication"
+    owner       = "devzwide"
+  }
+}
+
 variable "virtual_network_name" {
   description = "The name of the virtual network."
   type        = string
